d354aa83296af57b0afcd78d61afe57c8593b779
Romanov R <rr@pentestit.ru>
2022-12-23 12:15:18 +0700

    Added multipart/form-data; Added noew payload to CM

--- payloads.py
+++ payloads.py
@@ -5,45 +5,44 @@ import secrets
 
 
 class PayloadProcessing:
+    
     def __init__(self, json_path):
+        
         self._json_path = json_path
         with open(json_path) as opened_json:
             json_data = json.load(opened_json)['req'][0]
 
         # URL
         url = self.extract_value(json_data, 'URL')
-        self._url = None if (url is None or not url) else url.replace("%RND%", secrets.token_urlsafe(12))
+        self._url = None if not url else url.replace("%RND%", secrets.token_urlsafe(12))
 
         # ARGS
         args = self.extract_value(json_data, 'ARGS')
-        self._args = None if (args is None or not args) else args.replace("%RND%", secrets.token_urlsafe(12))
+        self._args = None if not args else args.replace("%RND%", secrets.token_urlsafe(12))
 
         # Body
         body = self.extract_value(json_data, 'Body')
-        self._body = None if (body is None or not body) else body.replace("%RND%", secrets.token_urlsafe(12))
+        self._body = None if not body else body.replace("%RND%", secrets.token_urlsafe(12))
 
         # Cookie
         cookie = self.extract_value(json_data, 'Cookie')
-        self._cookie = None if (cookie is None or not cookie) else cookie.replace("%RND%", secrets.token_urlsafe(12))
+        self._cookie = None if not cookie else cookie.replace("%RND%", secrets.token_urlsafe(12))
 
         # UA
         ua = self.extract_value(json_data, 'UA')
-        self._ua = None if (ua is None or not ua) else ua.replace("%RND%", secrets.token_urlsafe(12))
+        self._ua = None if not ua else ua.replace("%RND%", secrets.token_urlsafe(12))
 
         # Referer
         referer = self.extract_value(json_data, 'Referer')
-        self._referer = None if (referer is None or not referer) else referer.replace("%RND%", secrets.token_urlsafe(12))
+        self._referer = None if not referer else referer.replace("%RND%", secrets.token_urlsafe(12))
 
         # Headers
         headers = self.extract_value(json_data, 'Headers')
-        self._headers = None if (headers is None or not headers) \
-            else headers.replace("%RND%", secrets.token_urlsafe(12))
+        self._headers = None if not headers else headers.replace("%RND%", secrets.token_urlsafe(12))
 
         # Method
         method = self.extract_value(json_data, 'Method')
-        if method:
-            method.lower()
-        self._method = method
+        self._method = None if not method else method.lower()
 
         # Blocked (True if not set)
         blocked = self.extract_value(json_data, 'Blocked')
