e14c9b3ee121bde0af4bb03b6894a84ffd49778f
Romanov R <rr@pentestit.ru>
2023-01-18 20:06:43 +0700

    Table's design

--- tables.py
+++ tables.py
@@ -21,17 +21,17 @@ def table_get_result_details(fp, fn):
 
         if status == 'FP':
             col_1 = 10 * ' ' + 'FALSE POSITIVE PAYLOAD' + 10 * ' '
-            col_2 = 25 * ' ' + 'ZONE' + 25 * ' '
+            col_2 = 210 * ' ' + 'ZONE' + 25*' '
             table = PrettyTable([col_1, col_2])
             table.align[col_1] = 'l'
         elif status == 'FN':
             col_1 = 10 * ' ' + 'FALSE NEGATIVE PAYLOAD' + 10 * ' '
-            col_2 = 25 * ' ' + 'ZONE' + 25 * ' '
+            col_2 = 210 * ' ' + 'ZONE' + 25*' '
             table = PrettyTable([col_1, col_2])
             table.align[col_1] = 'l'
         else:
             col_1 = 10 * ' ' + 'PAYLOAD' + 10 * ' '
-            col_2 = 25 * ' ' + 'ZONE' + 25 * ' '
+            col_2 = 210 * ' ' + 'ZONE' + 25*' '
             table = PrettyTable([col_1, col_2])
             table.align[col_1] = 'l'
 
@@ -52,69 +52,77 @@ def table_get_result_summary(wb_result):
 
     # init
     payloads_summary_dict = {}
-    payloads_summary_list = []
-    table_headers = ['PAYLOAD TYPE', 'PASSED', 'NOT PASSED', 'FALSE POSITIVE', 'FALSE NEGATIVE', 'ERROR']
+    payloads_summary_list_fp = []
+    payloads_summary_list_fn = []
+    table_headers_fn = ['PAYLOAD TYPE', 10 * ' ' + 'PASSED', 10 * ' ' + 'BYPASSED', 10 * ' ' + 'ERROR']
+    table_headers_fp = ['TOTAL', 10 * ' ' + 'PASSED', 10 * ' ' + 'FALSED', 10 * ' ' + 'ERROR']
 
     # get payloads type list
     payloads_type_list = list(set(['/'.join(k.split(':')[0].split('/')[:-1]) for k in wb_result.keys()]))
     
     # create result dictionary by payloads type
     for payloads_type in payloads_type_list:
-        passed = len([k for k, v in wb_result.items() if k.startswith(payloads_type) and v == 'PASSED'])
-        error = len([k for k, v in wb_result.items() if k.startswith(payloads_type) and v == 'ERROR'])
-        fp = len([k for k, v in wb_result.items() if k.startswith(payloads_type) and v == 'FP'])
-        fn = len([k for k, v in wb_result.items() if k.startswith(payloads_type) and v == 'FN'])
-        summ = passed + error + fp + fn
+        
         k = payloads_type.split('/payload/')[1].split('/')[0]  # leave payload type only
-        payloads_summary_dict[k] = [summ, passed, error, fp, fn]
+        k_type = 'FP' if k == 'FP' else 'FN'
+
+        passed = len([k for k, v in wb_result.items() if k.startswith(payloads_type) and v == 'PASSED'])
+        fx = len([k for k, v in wb_result.items() if k.startswith(payloads_type) and v == k_type])
+        error = len([k for k, v in wb_result.items() if k.startswith(payloads_type) and v == 'ERROR'])        
+        total = passed + error + fx
+
+        payloads_summary_dict[k] = [total, passed, fx, error]
 
     # create table's body of the payloads
     for k in sorted(payloads_summary_dict.keys()):
         
         v = payloads_summary_dict[k]
-        
-        passed = str(v[1]) + ' (' + get_percent_str(v[1], v[0]) + '%)'
-        not_passed = str(v[0] - v[1]) + ' (' + get_percent_str(v[0] - v[1], v[0]) + '%)'
-        error = str(v[2]) + ' (' + get_percent_str(v[2], v[0]) + '%)'
-        fp = str(v[3]) + ' (' + get_percent_str(v[3], v[0]) + '%)'
-        fn = str(v[4]) + ' (' + get_percent_str(v[4], v[0]) + '%)'
-        
-        payloads_summary_list.append([
-            k,
-            passed,
-            not_passed,
-            fp,
-            fn,
-            error
-        ])
 
-    tp.banner('SUMMARY BY TYPE ', style='banner')
-    tp.table(payloads_summary_list, table_headers)
+        total = v[0]
+        passed = str(v[1]) + ' (' + get_percent_str(v[1], v[0]) + '%)'
+        fx = str(v[2]) + ' (' + get_percent_str(v[2], v[0]) + '%)'
+        error = str(v[3]) + ' (' + get_percent_str(v[3], v[0]) + '%)'
+        
+        if k == 'FP':
+            payloads_summary_list_fp.append([total, passed, fx, error])
+        else:
+            payloads_summary_list_fn.append([k, passed, fx, error])
 
     ##
-    # add all summary to result
+    # Print FP/FN tables
+    ##
+
+    tp.banner('FALSE NEGATIVE ', style='banner')
+    tp.table(payloads_summary_list_fn, table_headers_fn)
+
+    print('')
+    tp.banner('FALSE POSITIVE ', style='banner')
+    tp.table(payloads_summary_list_fp, table_headers_fp)
+
+    ##
+    # Add all summary to result
     ##
 
     # init
-    summ = 0
+    total = 0
     payloads_summary_list = []
-    table_headers = ['TOTAL', 'PASSED', 'NOT PASSED', 'FALSE POSITIVE', 'FALSE NEGATIVE', 'ERROR']
+    table_headers = ['TOTAL PAYLOADS', 5 * ' ' + 'PASSED', 5 * ' ' + 'NOT PASSED', 5 * ' ' + 'FALSE POSITIVE', 5 * ' ' + 'FALSE NEGATIVE', 5 * ' ' + 'ERROR']
 
     i = len([k for k, v in wb_result.items() if v == 'PASSED'])
     passed = str(i) + ' (' + get_percent_str(i, len(wb_result)) + '%)'
-    summ = summ + i
+    total = total + i
     
     i = len([k for k, v in wb_result.items() if v == 'ERROR'])
     error = str(i) + ' (' + get_percent_str(i, len(wb_result)) + '%)'
-    summ = summ + i
+    total = total + i
 
     i = len([k for k, v in wb_result.items() if v == 'FP'])
     fp = str(i) + ' (' + get_percent_str(i, len(wb_result)) + '%)'
-    summ = summ + i
+    total = total + i
 
     i = len([k for k, v in wb_result.items() if v == 'FN'])
     fn = str(i) + ' (' + get_percent_str(i, len(wb_result)) + '%)'
-    summ = summ + i
+    total = total + i
 
     i = len(wb_result) - len([k for k, v in wb_result.items() if v == 'PASSED'])
     not_passed = str(i) + ' (' + get_percent_str(i, len(wb_result)) + '%)'
@@ -128,9 +136,10 @@ def table_get_result_summary(wb_result):
         error
     ])
 
+    print('')
     tp.banner('TOTAL SUMMARY ', style='banner')
     tp.table(payloads_summary_list, table_headers)
 
     # summary validation
-    if summ != len(wb_result):
-        print('ERROR: Summary processing is incorrect ({} != {})'.format(summ, len(wb_result)))
+    if total != len(wb_result):
+        print('ERROR: Summary processing is incorrect ({} != {})'.format(total, len(wb_result)))
