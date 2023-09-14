ab480e2ddc43c042dd792671cf95458e38271c22
James Conlan <10931523+JamesConlan96@users.noreply.github.com>
2023-09-14 12:03:55 +0100

    Added setup.py to support install using pipx

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import re
 import sys
 
 from requests.exceptions import MissingSchema
-from bypass import WAFBypass
+from utils.bypass import WAFBypass
 from urllib3 import connectionpool, poolmanager
 
 
@@ -44,140 +44,145 @@ def get_help():
     print("--exclude-dir - exclude the payload's directory (e.g., --exclude-dir='FP'). Multiple use is allowed.")
     
 
-# increasing max pool size
-patch_http_connection_pool(maxsize=50)
+def main():
+    # increasing max pool size
+    patch_http_connection_pool(maxsize=50)
 
-# init params
-host = ''
-proxy = {}
-headers = {}
-threads = 5
-timeout = 30
-wb_result = {}
-wb_result_json = False
-details = False
-exclude_dir = []
+    # init params
+    host = ''
+    proxy = {}
+    headers = {}
+    threads = 5
+    timeout = 30
+    wb_result = {}
+    wb_result_json = False
+    details = False
+    exclude_dir = []
 
-# set user-agent
-headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    # set user-agent
+    headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
 
-# Processing args from cmd
-try:
+    # Processing args from cmd
+    try:
 
-    # read args from input
-    launch_args = sys.argv[1:]
+        # read args from input
+        launch_args = sys.argv[1:]
 
-    # options
-    launch_args_options = [
-        'help', 'host=', 'proxy=', 'header=', 'user-agent=', 'block-code=', 'threads=', 'timeout=', 'json-format', 'details', 'exclude-dir='
-    ]
+        # options
+        launch_args_options = [
+            'help', 'host=', 'proxy=', 'header=', 'user-agent=', 'block-code=', 'threads=', 'timeout=', 'json-format', 'details', 'exclude-dir='
+        ]
 
-    # parsing args
-    block_code = {}
-    optlist, values = getopt.getopt(launch_args, '', launch_args_options)
+        # parsing args
+        block_code = {}
+        optlist, values = getopt.getopt(launch_args, '', launch_args_options)
+        
+        for k, v in optlist:
+            
+            if k == '--help':
+                get_help()
+                sys.exit()
+            
+            if k == '--host':
+                host = str(v).lower()
+                # check host's schema
+                if not re.search(r'^https?://', host):
+                    host = 'http://' + host
+            
+            elif k == '--proxy':
+                proxy = str(v).lower()
+            
+            elif k == '--header':
+                hname, hval = str(v).split(':')
+                hname = hname.strip()
+                hval = hval.strip()
+                headers[hname] = hval
+
+            elif k == '--user-agent':
+                headers['User-Agent'] = v
+            
+            elif k == '--block-code':
+                block_code[int(v)] = True
+
+            elif k == '--threads':
+                threads = int(v)
+
+            elif k == '--timeout':
+                timeout = int(v)
+
+            elif k == '--json-format':
+                wb_result_json = True
+
+            elif k == '--details':
+                details = True
+
+            elif k == '--exclude-dir':
+                exclude_dir.extend(v.replace(',', ' ').split())
+        
+        if len(block_code) == 0:
+            block_code[403] = True
+
+        # convert to uppercase
+        exclude_dir = [x.upper() for x in exclude_dir]
+
+    except Exception as e:
+        print('An error occurred while processing the TARGET/PROXY: {}'.format(e))
+        sys.exit()
+
+    # check host
+    if not host:
+        print('An error occurred: the TARGET is not set')
+        get_help()
+        sys.exit()
+
+    # print basic info
+    if not wb_result_json:
+
+        print('')
+        print('##')
+        print('# Target:       {}'.format(host))
+        print('# Proxy:        {}'.format(proxy)) if len(proxy) else print('# Proxy:')
+        print('# Timeout:      {}s'.format(timeout))
+        print('# Threads:      {}'.format(threads))
+        print('# Block code:   {}'.format(list(block_code.keys())[0]))
+        print('# Exclude dirs: {}'.format(' '.join(exclude_dir)))
+
+        if len(headers) > 0:
+            for k, v in headers.items():
+                if k.lower() == 'user-agent':
+                    continue
+                else:    
+                    print('# Headers:      {}: {}'.format(k, v))
+
+        print('# User-Agent:   {}'.format(headers['User-Agent']))
+        print('##')
+        print('')
+
+    # update result dictionary
+    else:
+
+        wb_result['TARGET'] = host
+        wb_result['PROXY'] = proxy
+        wb_result['HEADERS'] = headers
+        wb_result['BLOCK-CODE'] = list(block_code.keys())
+        wb_result['THREADS'] = threads
+        wb_result['TIMEOUT'] = timeout
+        wb_result['EXCLUDE-DIR'] = exclude_dir
+
+    # launch WAF Bypass
+    waf_bypass = WAFBypass(host, proxy, headers, block_code, timeout, threads, wb_result, wb_result_json, details, exclude_dir)
+
+    try:
+        waf_bypass.start()
+    except KeyboardInterrupt:
+        print('\nKeyboard Interrupt')
+
+    except MissingSchema:
+        print('An error occurred: protocol is not set for TARGET or PROXY')
+
+    if not wb_result_json:
+        print('')
+
     
-    for k, v in optlist:
-        
-        if k == '--help':
-            get_help()
-            sys.exit()
-        
-        if k == '--host':
-            host = str(v).lower()
-            # check host's schema
-            if not re.search(r'^https?://', host):
-                host = 'http://' + host
-        
-        elif k == '--proxy':
-            proxy = str(v).lower()
-        
-        elif k == '--header':
-            hname, hval = str(v).split(':')
-            hname = hname.strip()
-            hval = hval.strip()
-            headers[hname] = hval
-
-        elif k == '--user-agent':
-            headers['User-Agent'] = v
-        
-        elif k == '--block-code':
-            block_code[int(v)] = True
-
-        elif k == '--threads':
-            threads = int(v)
-
-        elif k == '--timeout':
-            timeout = int(v)
-
-        elif k == '--json-format':
-            wb_result_json = True
-
-        elif k == '--details':
-            details = True
-
-        elif k == '--exclude-dir':
-            exclude_dir.extend(v.replace(',', ' ').split())
-    
-    if len(block_code) == 0:
-        block_code[403] = True
-
-    # convert to uppercase
-    exclude_dir = [x.upper() for x in exclude_dir]
-
-except Exception as e:
-    print('An error occurred while processing the TARGET/PROXY: {}'.format(e))
-    sys.exit()
-
-# check host
-if not host:
-    print('An error occurred: the TARGET is not set')
-    get_help()
-    sys.exit()
-
-# print basic info
-if not wb_result_json:
-
-    print('')
-    print('##')
-    print('# Target:       {}'.format(host))
-    print('# Proxy:        {}'.format(proxy)) if len(proxy) else print('# Proxy:')
-    print('# Timeout:      {}s'.format(timeout))
-    print('# Threads:      {}'.format(threads))
-    print('# Block code:   {}'.format(list(block_code.keys())[0]))
-    print('# Exclude dirs: {}'.format(' '.join(exclude_dir)))
-
-    if len(headers) > 0:
-        for k, v in headers.items():
-            if k.lower() == 'user-agent':
-                continue
-            else:    
-                print('# Headers:      {}: {}'.format(k, v))
-
-    print('# User-Agent:   {}'.format(headers['User-Agent']))
-    print('##')
-    print('')
-
-# update result dictionary
-else:
-
-    wb_result['TARGET'] = host
-    wb_result['PROXY'] = proxy
-    wb_result['HEADERS'] = headers
-    wb_result['BLOCK-CODE'] = list(block_code.keys())
-    wb_result['THREADS'] = threads
-    wb_result['TIMEOUT'] = timeout
-    wb_result['EXCLUDE-DIR'] = exclude_dir
-
-# launch WAF Bypass
-waf_bypass = WAFBypass(host, proxy, headers, block_code, timeout, threads, wb_result, wb_result_json, details, exclude_dir)
-
-try:
-    waf_bypass.start()
-except KeyboardInterrupt:
-    print('\nKeyboard Interrupt')
-
-except MissingSchema:
-    print('An error occurred: protocol is not set for TARGET or PROXY')
-
-if not wb_result_json:
-    print('')
+if __name__ == "__main__":
+    main()
