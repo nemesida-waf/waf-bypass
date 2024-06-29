5c741445e34233e320aec0f7aa682db53149d594
Romanov R <rr@pentestit.ru>
2024-06-29 08:00:16 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ setup(
     packages=['utils'],
     py_modules=['main'],
     install_requires=[
-        'attrs==22.1.0',
-        'chardet==5.1.0',
-        'colorama==0.4.6',
-        'curlify==2.2.1',
-        'requests==2.31.0',
-        'tableprint==0.9.1',
-        'yarl==1.8.2'
+        'attrs==22.1.0,
+        'chardet==5.1.0,
+        'colorama==0.4.6,
+        'curlify==2.2.1,
+        'requests==2.32.3,
+        'tableprint==0.9.1,
+        'yarl==1.8.2,
     ],
     python_requires='>=3.0.0',
     entry_points={
