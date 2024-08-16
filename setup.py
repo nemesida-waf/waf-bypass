d8ffd2ded6f232620cdbb37e96d18f8b8c38e6f4
Romanov R. <rr@pentestit.ru>
2024-08-16 09:11:42 +0300

    req. upd

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ setup(
     packages=['utils'],
     py_modules=['main'],
     install_requires=[
-        'attrs==22.1.0,
-        'chardet==5.1.0,
-        'colorama==0.4.6,
-        'curlify==2.2.1,
-        'requests==2.32.3,
-        'tableprint==0.9.1,
-        'yarl==1.8.2,
+        'attrs==24.2.0',
+        'chardet==5.2.0',
+        'colorama==0.4.6',
+        'curlify==2.2.1',
+        'requests==2.32.3',
+        'tableprint==0.9.1',
+        'yarl==1.9.4'
     ],
     python_requires='>=3.9.0',
     entry_points={
